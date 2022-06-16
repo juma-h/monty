70af3701e54f5beda3329801a2edd9153c434a05
lewah <ingrid.lewah@gmail.com>
2022-06-16 21:14:59 +0300

    0x19. C - Stacks, Queues - LIFO, FIFO

--- bytecodes/000.m
+++ bytecodes/000.m
@@ -1,9 +1,12 @@
-push 0$
-push 1$
-push 2$
-  push 3$
-                   pall    $
-push 4$
-    push 5    $
-      push    6        $
-pall$
+push 0
+push 1
+push 52 fsdg
+  push 3
+  
+                   pall    
+push 4
+    push 5   
+    gdgfr gdf g 
+      push    6        
+pall
+gdgfr gdf g
--- bytecodes/19.m
+++ bytecodes/19.m
@@ -1,5 +1,7 @@
+sub
 push 1
 push 2
+push -10
 push 3
-push 
+sub
 pall
